3d4b78407dcc17bca6ece0a98b3a1bdd26e673ae
elebouli <elise.maria.le.boulicaut@cern.ch>
2023-03-07 00:04:50 +0100

    Attempt at running jobs on condor.

---
+++ truth-studies/NeutrinoReconstruction/condor/execute_job.sh
@@ -0,0 +1,3 @@
+source /eos/user/e/elebouli/BSM4tops/FourTopsAnalysis/setup-scripts/PythonGNN/bin/activate
+python /eos/user/e/elebouli/BSM4tops/FourTopsAnalysis/models_save/NeutrinoReconstruction/main_condor.py $1
+
